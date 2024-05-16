4a36506ed151f3747194022b4b7671a49232fb28
thomasmendez <thomasmendez01@gmail.com>
2024-05-16 01:18:33 -0500

    Added backend files

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # todo-mern
+
+### Frontend
+
+`yarn dev`
+
+### Backend
+
+#### Run Local Dev Environment 
+
+`yarn build`
+
+`yarn start`
+
+#### Run Dockerize Container
+
+`docker compose up`
--- backend/index.ts
+++ backend/index.ts
@@ -6,10 +6,10 @@ server.get('/ping', async (request, reply) => {
   return 'pong\n'
 })
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
   }
-  console.log(`Server listening at ${address}`)
+  console.log(`Server started...`)
 })
